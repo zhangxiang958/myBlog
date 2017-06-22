4f5377a07eed1ddb433da9809bcda80fd7d4684b
zhangxiang <958033967@qq.com>
2017-06-22 22:54:57 +0800

    add markdown

--- src/views/Blog/index.jsx
+++ src/views/Blog/index.jsx
@@ -2,15 +2,45 @@ import React      from 'react';
 import Layout     from '../../components/Layout/index.jsx';
 import Content    from '../../components/Content/index.jsx';
 import PostItem   from '../../components/PostItem/index.jsx';
-import Pagination from '../../components/Pagination/index.jsx'
+import Pagination from '../../components/Pagination/index.jsx';
+import 'whatwg-fetch';
+// import githubMarkdown from '../../static/css/github-markdown.css';
+// import prism from '../../static/css/prism.css';
+// import style from '../../static/css/style.css';
 
 class Blog extends React.Component {
     constructor(...props){
         super(...props);
+
+        this.state = {
+            postDetail: {}
+        }
     };
     
     fetchArticle(){
 
+        fetch('http://localhost:8080/')
+        .then(function(response){
+
+            return response.text();
+        })
+        .then((body) => {
+            let fuck = document.querySelector('.fuck');
+            console.log(typeof body);
+            // fuck.innerHTML = body;
+            // this.setState({
+            //     postDetail: {
+            //         __html: body
+            //     }
+            // });
+            return {
+                __html: body
+            };
+        });
+    }
+
+    componentDidMount() {
+        // this.fetchArticle();
     }
 
     render() {
@@ -19,6 +49,7 @@ class Blog extends React.Component {
                 <section id="posts" className="posts">
                     <PostItem />
                 </section>
+                <div className="fuck" dangerouslySetInnerHTML={this.fetchArticle()} />
                 <Pagination />
             </Content>
         );
